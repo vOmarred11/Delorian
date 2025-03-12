78e9495d0a9aac9836e855674d14b3f39196d9e3
vOmarred11 <canziansonia@gmail.com>
2025-03-12 14:42:11 +0100

    Update makeslice.go

--- hud/stc/makeslice.go
+++ hud/stc/makeslice.go
@@ -1,8 +1,12 @@
 package stc
 
+import "github.com/sandertv/gophertunnel/minecraft/protocol"
 func slice() string {
 	return "stc error-1"
 }
 func make() {
 	slice()
+}
+type stc struct{
+	upon []protocol.Command
 }
